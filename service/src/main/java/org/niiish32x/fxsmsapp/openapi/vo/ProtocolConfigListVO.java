b22a6e9f8c32fede84f36fc903d4ee9235258470
nishenghao <1159417019@qq.com>
2024-12-04 15:46:07 +0800

    protocol vo

---
+++ service/src/main/java/org/niiish32x/fxsmsapp/openapi/vo/ProtocolConfigListVO.java
@@ -0,0 +1,19 @@
+package org.niiish32x.fxsmsapp.openapi.vo;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ProtocolConfigListVO
+ *
+ * @author shenghao ni
+ * @date 2024.12.04 15:26
+ */
+@Data
+public class ProtocolConfigListVO {
+
+
+    private List<Map> protocols;
+}
---
+++ service/src/main/java/org/niiish32x/fxsmsapp/openapi/vo/ProtocolTemplateVO.java
@@ -0,0 +1,24 @@
+package org.niiish32x.fxsmsapp.openapi.vo;
+
+import lombok.Data;
+
+/**
+ * ProtocolTemplateVO
+ *
+ * @author shenghao ni
+ * @date 2024.12.04 15:27
+ */
+@Data
+public class ProtocolTemplateVO {
+
+
+    private String name;
+
+    private String i18nKey;
+
+    private String code;
+
+    private String template;
+
+    private String description;
+}
