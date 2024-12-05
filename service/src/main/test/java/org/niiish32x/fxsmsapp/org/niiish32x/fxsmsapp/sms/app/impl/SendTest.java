895a66c39dba5049c5648dc8d73f81784d6e06e9
nishenghao <1159417019@qq.com>
2024-12-05 10:30:09 +0800

    单测

--- service/src/main/test/java/org/niiish32x/fxsmsapp/org/niiish32x/fxsmsapp/sms/app/impl/SendTest.java
+++ service/src/main/test/java/org/niiish32x/fxsmsapp/org/niiish32x/fxsmsapp/sms/app/impl/SendTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.niiish32x.fxsmsapp.result.Result;
 import org.niiish32x.fxsmsapp.sms.app.SendSMSCService;
+import org.niiish32x.fxsmsapp.sms.app.impl.SendSMSCServiceImpl;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,13 +17,13 @@ import javax.annotation.Resource;
  * @date 2024.12.05 10:21
  */
 
-@SpringBootTest
+@SpringBootTest(classes ={SendSMSCService.class, SendSMSCServiceImpl.class})
 @RunWith(SpringRunner.class)
 public class SendTest {
     @Resource
     SendSMSCService sendSMSCService;
 
-    String number = "x";
+    String number = "919747934655";
     String text = "text";
     @Test
     public void sendTest() {
