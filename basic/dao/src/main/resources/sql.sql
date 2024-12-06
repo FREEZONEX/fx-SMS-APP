835cf8bfbc7e3c74cde2d41782cd24896acc8672
nishenghao <1159417019@qq.com>
2024-12-06 15:27:52 +0800

    restruct

--- basic/dao/src/main/resources/sql.sql
+++
@@ -1,9 +0,0 @@
- use sms;
-
-create table local_message(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    message_content varchar(500) not null ,
-    status varchar(20) not null default 'NEW',
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp on update current_timestamp
-)
